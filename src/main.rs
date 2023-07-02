205dca5e4dad1cd022c3c0a0e9286e715857d668
Rahul Ghosal <rdghosal@gmail.com>
2023-07-02 17:45:04 -0400

    feat: make port for health check dynamic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call `.await` on our Server
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
